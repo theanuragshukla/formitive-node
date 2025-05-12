22aa962c05f4b1a7818fe409aedd3938d71f91a3
theanuragshukla <tachyonanurag@gmail.com>
2025-05-12 12:29:39 +0000

    feat: socket url

--- ui/src/data/socket.js
+++ ui/src/data/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 export const connectSocket = () => {
-  const url = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
+  const url = process.env.REACT_APP_SERVER_URL ||  `${window.location.origin}:8000` ;
   const socket = io(url, {
     withCredentials: true,
   });
